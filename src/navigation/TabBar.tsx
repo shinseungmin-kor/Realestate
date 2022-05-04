69474698e669a9cf6d3d531d4538aee65afee38a
신승민 <shinseungmin10@gmail.com>
2022-05-04 19:22:25 +0900

    이미지추가

--- src/navigation/TabBar.tsx
+++ src/navigation/TabBar.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import ImageSet from '../images';
 
 const TabBar = (props: any) => {
+  const {state, descriptors, navigation} = props;
+
+  const {
+    home,
+    homeActive,
+    lounge,
+    loungeActive,
+    myPage,
+    myPageActive,
+    party,
+    partyActive,
+    judge,
+    judgeActive,
+    affinity,
+    affinityActive,
+  } = ImageSet.BottomTab;
+
+  const getIcon = (name: string, focused: boolean) => {
+    if (name === 'Daily') {
+      if (!focused) {
+        return home;
+      }
+      return homeActive;
+    } else if (name === 'Affinity') {
+      if (!focused) {
+        return affinity;
+      }
+      return affinityActive;
+    } else if (name === 'Party') {
+      if (!focused) {
+        return party;
+      }
+      return partyActive;
+    } else if (name === 'Lounge') {
+      if (!focused) {
+        return lounge;
+      }
+      return loungeActive;
+    } else if (name === 'HiPick') {
+      if (!focused) {
+        return judge;
+      }
+      return judgeActive;
+    } else if (name === 'MyPage') {
+      if (!focused) {
+        return myPage;
+      }
+      return myPageActive;
+    }
+  };
+
   return (
     <View>
       <Text>hello</Text>
